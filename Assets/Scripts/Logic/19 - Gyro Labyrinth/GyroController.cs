2d66404c15aafe6cd5fe6ced53eb3479ecacfaf5
am7249 <its.vencel@gmail.com>
2023-02-25 22:18:51 +0100

    Gyro level complete

--- Assets/Scripts/Logic/19 - Gyro Labyrinth/GyroController.cs
+++ Assets/Scripts/Logic/19 - Gyro Labyrinth/GyroController.cs
@@ -2,27 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GyroController : MonoBehaviour
-{
+public class GyroController : MonoBehaviour {
     private Gyroscope gyro;
     private Rigidbody2D rb;
 
     [SerializeField] private float velocity = 50.0f;
     [SerializeField] private float drag = 1.0f;
+    [SerializeField] private float tiltTreshold = 0.05f;
 
     private Vector2 calucaltedVelocity = Vector2.zero;
 
-    void Start()
-    {
+    private bool canMove = true;
+
+    void Start() {
         rb = GetComponent<Rigidbody2D>();
         gyro = Input.gyro;
         gyro.enabled = true;
     }
 
-    void Update()
-    {
-        float xGain = Mathf.Abs(gyro.gravity.x) > 0.05f ? gyro.gravity.x : 0f;
-        float yGain = Mathf.Abs(gyro.gravity.y) > 0.05f ? gyro.gravity.y : 0f;
+    void Update() {
+        if (!canMove) return;
+
+        float xGain = Mathf.Abs(gyro.gravity.x) > tiltTreshold ? gyro.gravity.x : 0f;
+        float yGain = Mathf.Abs(gyro.gravity.y) > tiltTreshold ? gyro.gravity.y : 0f;
 
         calucaltedVelocity.x = Mathf.Lerp(calucaltedVelocity.x, xGain, drag * Time.deltaTime);
         calucaltedVelocity.y = Mathf.Lerp(calucaltedVelocity.y, yGain, drag * Time.deltaTime);
